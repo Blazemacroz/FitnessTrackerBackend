0761bfe4f3b8a663e84cc4883ea41c2f70b5f590
Sage <blazemacroz@gmail.com>
2023-07-19 20:27:10 -0500

    connection string

--- db/client.js
+++ db/client.js
@@ -1,7 +1,8 @@
 const { Pool } = require('pg');
 require('dotenv').config;
 const { localHostConnection } = require('../localhostconnect.js')
+const connectionString = process.env.DATABASE_URL || localHostConnection;
 // This Will not work when deployed
-const client = new Pool(localHostConnection);
+const client = new Pool(connectionString);
 
 module.exports = client;
