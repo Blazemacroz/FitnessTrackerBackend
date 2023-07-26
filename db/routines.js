ebe502a75b03f8e70036d19e95133e0e35c5db4e
Noah Mauer <noahmauer1998@gmail.com>
2023-07-25 20:25:04 -0400

    Finished getAllRoutines()

--- db/routines.js
+++ db/routines.js
@@ -2,18 +2,18 @@ const client = require("./client");
 
 async function createRoutine({ creatorId, isPublic, name, goal }) {
   try {
-  const { rows: [routine] } = await client.query(`
+    const { rows: [routine] } = await client.query(`
     INSERT INTO routines("creatorId", "isPublic", name, goal)
     VALUES ($1, $2, $3, $4)
     ON CONFLICT (name) DO NOTHING
     RETURNING *;
   `, [creatorId, isPublic, name, goal]);
-  if (!routine) {
-    throw Error;
-  } else {
-  console.log("routine: ", routine);
-  return routine;
-  }
+    if (!routine) {
+      throw Error;
+    } else {
+      console.log("routine: ", routine);
+      return routine;
+    }
   } catch (err) {
     console.error(err)
   }
@@ -23,56 +23,66 @@ async function createRoutine({ creatorId, isPublic, name, goal }) {
 
 async function getRoutinesWithoutActivities() {
   try {
-  const { rows: routines } = await client.query(`
+    const { rows: routines } = await client.query(`
     SELECT * FROM routines;
   `);
-  if (!routines) {
-    throw Error;
-  } else {
-  console.log("all routines: ", routines);
-  return routines;
-  }  
-} catch (err) {
-  console.log(err)
+    if (!routines) {
+      throw Error;
+    } else {
+      console.log("all routines: ", routines);
+      return routines;
+    }
+  } catch (err) {
+    console.log(err)
+  }
 }
-};
 
-async function getAllRoutines() { 
-try {
-  const { rows: routines } = await client.query(`
-  SELECT * FROM routines;
+async function getAllRoutines() {
+  try {
+    const { rows: routines } = await client.query(`
+  SELECT routines.id, routines."creatorId", routines."isPublic",
+  routines.name, routines.goal, users.username AS "creatorName"
+  FROM routines
+  JOIN users ON routines."creatorId"=users.id
+  ;
   `)
-  console.log("getAllRoutines(before)" , routines);
-  if (!routines) {
-    throw Error;
-  } else {
-const activitiesPromise = routines.map( async (routine) => {
-  const { rows: activitesReference } = await client.query(`
-  SELECT * FROM routine_activities
-  WHERE "routineId"=$1;
+    console.log("getAllRoutines(before)", routines);
+    if (!routines) {
+      throw Error;
+    } else {
+      const activitiesPromise = routines.map(async (routine) => {
+        const { rows: activitesReference } = await client.query(`
+          SELECT * FROM routine_activities
+          WHERE "routineId"=$1;
   `, [routine.id])
-  if (activitesReference.length > 0) {
-    const activities = activitesReference.map( async (reference) => {
-      const { rows: routineActivities } = await client.query(`
-      SELECT * FROM activities
-      WHERE "routineId"=$1;
-      `, [reference.routineId])
-return routineActivities;
-    })
-  routine.activities = activities;
-  return routine;
-  } else {
-    return routine
-  }
-})
-const routinesWithActivities = await Promise.all(activitiesPromise);
+        if (activitesReference.length > 0) {
+          const activities = activitesReference.map(async (reference) => {
+            const { rows: [routineActivity] } = await client.query(`
+              SELECT activities.id, activities.name, activities.description
+              FROM activities
+              WHERE id=$1;
+      `, [reference.activityId])
+            routineActivity.duration = reference.duration;
+            routineActivity.count = reference.count;
+            routineActivity.routineId = reference.routineId;
+            routineActivity.routineActivityId = reference.id;
+            console.log("routineActivities: ", routineActivity);
+            return routineActivity;
+          })
+          routine.activities = await Promise.all(activities);
+          return routine;
+        } else {
+          return routine;
+        }
+      })
+      const routinesWithActivities = await Promise.all(activitiesPromise);
 
-    console.log("getAllRoutines (after)", routinesWithActivities)
-    return routinesWithActivities;
+      console.log("getAllRoutines (after)", routinesWithActivities)
+      return routinesWithActivities;
+    }
+  } catch (err) {
+    console.error(err)
   }
-} catch (err) {
-  console.error(err)
-}
 }
 
 async function getAllPublicRoutines() { }
