c573b80995958530dcd729e7d3f41d48c622c4a8
Noah Mauer <noahmauer1998@gmail.com>
2023-08-02 15:01:59 -0400

    Troubleshooting fly.io databases connection

--- db/client.js
+++ db/client.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 const connectionString = process.env.DATABASE_URL;
 
 const client = new Pool({
-    connectionString,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
+    connectionString
+    // ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
 });
 
 module.exports = client;
