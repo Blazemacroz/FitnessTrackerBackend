cfb6279def9a662d62ad16befe3f85765d7637ab
Noah Mauer <noahmauer1998@gmail.com>
2023-08-02 13:05:53 -0400

    Executed flyctl launch

--- db/client.js
+++ db/client.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 require('dotenv').config;
 const { localHostConnection } = require('../localhostconnect.js')
 const connectionString = process.env.DATABASE_URL || localHostConnection;
-// This Will not work when deployed
+
 const client = new Pool({
     connectionString,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
